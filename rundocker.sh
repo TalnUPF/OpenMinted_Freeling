5e639994f12d7813f00a4503011f922ab0009f97
Joan Codina <joan.codina@upf.edu>
2018-03-15 10:45:19 +0100

    a push to force the docker build command on dockerhub

--- rundocker.sh
+++ rundocker.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker run -V xenial_freeling-java
+#this bash is to store the correct command to run the docker (from outside the docker ;-) 
+docker pull taln/openminted_freeling
+docker run -V openminted_freeling
