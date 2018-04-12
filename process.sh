68d49be4f86ea4deb8750d379b6ea7014724090a
Joan Codina <joancf@users.noreply.github.com>
2018-04-12 23:54:09 +0200

    update in run  parameters

--- process.sh
+++ process.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/bin/bash
 
 POSITIONAL=()
 while [[ $# -gt 0 ]]
@@ -8,7 +7,7 @@ key="$1"
 
 case $key in
     --input)
-    INPUT="$2"
+    INPUT="$2/*.xmi"
     shift # past argument
     shift # past value
     ;;
@@ -37,8 +36,4 @@ echo LANG    = "${LANG}"
 export LD_LIBRARY_PATH=/usr/local/share/freeling/APIs/java/
 cd UIMA
 export CLASSPATH="target/FreeLingWrapper-0.1-SNAPSHOT.jar":$(<classPath.txt)
-java -Xmx450m -cp $CLASSPATH  edu.upf.taln.uima.freeling.FreelingXMIReaderWriter  '/input/*.txt' /output $1 xmi
-#java -Xmx450m -cp $CLASSPATH  edu.upf.taln.uima.freeling.FreelingXMIReaderWriter  '/input/*.txt' /output $1 xmi
-#java -Xmx450m -cp $CLASSPATH  edu.upf.taln.uima.freeling.FreelingXMIReaderWriter  'input/*.txt' output auto txt
-#java -Xmx450m -cp $CLASSPATH  edu.upf.taln.uima.freeling.FreelingXMIReaderWriter  'input/nb*.txt' output nb txt
-#java -Xmx450m -cp $CLASSPATH  edu.upf.taln.uima.freeling.FreelingXMIReaderWriter  'input/ru*.txt' output ru txt
+java -Xmx450m -cp $CLASSPATH  edu.upf.taln.uima.freeling.FreelingXMIReaderWriter ${INPUT} ${OUTPUT}  ${LANG} xmi
